d5e006ea8789d11572d64615cead2f0a7f83f986
Kevin Luo <kvn.luo@gmail.com>
2020-10-24 16:27:35 -0400

    display response data through route param

--- src/components/post.js
+++ src/components/post.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Post extends Component {
   state = {
-    id: null
+    post: null
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     console.log(this.props);
     let id = this.props.match.params.post_id
-    this.setState({id:id})
+    let res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+    this.setState({
+      post:res.data
+    })
+    console.log(this.state);
   }
 
   render() {
+    const post = this.state.post ? (
+      <div className="post">
+        <h4 className="center">{this.state.post.title}</h4>
+        <p>{this.state.post.body}</p>
+      </div>
+    ) : (
+      <div className="center">Loading post...</div>
+    )
+
     return (
       <div className="container">
-        <h4>{this.state.id}</h4>
+        <h4>{post}</h4>
       </div>
     )
   }
