910d3a78e28ba803a83cb088ee9fe0e45a84ce0d
Kevin Luo <kvn.luo@gmail.com>
2020-11-07 01:51:43 -0500

    react app basic

--- src/components/home.js
+++ src/components/home.js
@@ -14,7 +14,6 @@ class Home extends Component {
     let search = Math.floor(Math.random() * 18) + 1;
     let res = await axios.get(`https://pokeapi.co/api/v2/type/${search}`)
 
-    console.log(res);
     this.setState({
       type: res.data.name,
       pokemon: res.data.pokemon
@@ -24,19 +23,19 @@ class Home extends Component {
   render () {
 
     const { type, pokemon } = this.state;
-    const header = <h4 className="center">{type} type</h4>;
+    const header = <h4 className="center">{ type } type</h4>;
 
     const ListLength = pokemon.length;
 
     const pokeList = (ListLength) ? (
       pokemon.map(pokemon => {
         return (
-          <div className="post card" key={pokemon.pokemon.name}>
+          <div className="post card" key={ pokemon.pokemon.name }>
             <img src={ Pokeball } alt="pokeball"/>
             <div className="card-content">
-              <span className="card-title red-text">{pokemon.pokemon.name}</span>
-              <Link to={`/${pokemon.pokemon.name}`}>
-                <p>{pokemon.pokemon.name} stats</p>
+              <span className="card-title red-text">{ pokemon.pokemon.name }</span>
+              <Link to={ `/${ pokemon.pokemon.name }` }>
+                <p>{ pokemon.pokemon.name } stats</p>
               </Link>
             </div>
           </div>
